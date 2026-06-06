b70207c0cd3077ecc70aaf5310c59f94ad916988
h6o <48004352+newbee1939@users.noreply.github.com>
2026-06-06 10:36:37 +0900

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        const numsMap = new Map<number, number>();
+        for (let num of nums) {
+            numsMap.set(num, (numsMap.get(num) || 0)+1);
+        }
+        // 結果が マイナス ➔ a を b より前にする
+        // 結果が プラス ➔ b を a より前にする（a を後ろにする）
+        // 結果が ゼロ ➔ 順番を変えない
+        const numsArray = Array.from(numsMap).sort((a, b) => b[1] - a[1]);
+
+        return numsArray.slice(0, k).map(x => x[0]);
+    }
+}
