5318743e5138460c3b4f55213016d234cc73068b
h6o <48004352+newbee1939@users.noreply.github.com>
2026-08-01 14:32:28 +0900

    Add: single-number - submission-5

---
+++ Data Structures & Algorithms/single-number/submission-5.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    singleNumber(nums: number[]): number {
+        // sortして横の値と比較
+        // 数字を小さい順に並べる
+        const sortedNums = nums.sort((a, b) => a - b);
+        
+        // 1つ以上スキップする可能性がある場合、whileを使えばいい
+        let i = 0;
+        while (i < sortedNums.length) {
+            if (nums[i] === nums[i + 1]) {
+                // 検査した2つ分を一気にスキップ
+                i = i + 2;
+                continue;
+            } else {
+                // 重複がないということなので、これが答え
+                return nums[i];
+            }
+        }
+    }
+}
